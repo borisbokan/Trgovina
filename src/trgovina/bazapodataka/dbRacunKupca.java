ce14dadf8ef021f4643d835f9f0ef166cd4ae480
borcha <borcha@borcha-hp>
2019-01-12 22:17:40 +0100

    Uradjen fajl dbRacunKupca

---
+++ src/trgovina/bazapodataka/dbRacunKupca.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trgovina.bazapodataka;
+
+import trgovina.bazapodataka.interfejsi.IRacunKupca;
+import trgovina.model.pcg_kupac.Trosak;
+import trgovina.model.pcg_kupac.Uplata;
+
+/**
+ *
+ * @author borcha
+ */
+public class dbRacunKupca implements IRacunKupca{
+
+    @Override
+    public void SnimiUplatu(Uplata _uplata) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void IspraviUplatu(Uplata _uplata) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void IspraviUplatu(int id, Uplata _uplata) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ObrisiUplatu(Uplata _uplata) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ObrisiUplatu(int _idUplate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void SnimiTrosak(Trosak _trosak) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void IspraviTrosak(Trosak _trosak) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void IspraviTrosak(int id, Trosak _trosak) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ObrisiTrosak(Trosak _trosak) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ObrisiTrosak(int _idTrosak) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+        
+        
+    
+    
+}
